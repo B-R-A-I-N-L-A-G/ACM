1194315e59fc849d06d612b92be614564a8e5896
dev-null-undefined <martinkos007@gmail.com>
2023-03-02 21:12:37 +0100

    week-02-dora-and-search: sliding window

--- week-02/dora-and-search/main.cpp
+++ week-02/dora-and-search/main.cpp
@@ -42,6 +42,52 @@ using fast = std::int_fast32_t;
 namespace rng = std::ranges;
 #define int ll
 //endregion
+
+void solve() {
+    int l; cin >> l;
+    vector<int> nums(l);
+
+    for (int j = 0; j < l; ++j) cin >> nums[j];
+
+    int lowIdx = 0;
+    int hiIdx = l - 1;
+
+    int min = 1;
+    int max = l;
+
+    bool changed = true;
+    while (changed) {
+        changed = false;
+
+        while (nums[lowIdx] == min || nums[lowIdx] == max) {
+            if (nums[lowIdx] == min) min ++;
+            else max--;
+
+            lowIdx++;
+
+            if (lowIdx > hiIdx) {
+                cout << -1 << endl;
+                return;
+            }
+        }
+
+        while (nums[hiIdx] == min || nums[hiIdx] == max) {
+            if (nums[hiIdx] == min) min ++;
+            else max--;
+
+            hiIdx--;
+            changed = true;
+
+            if (lowIdx > hiIdx) {
+                cout << -1 << endl;
+                return;
+            }
+        }
+    }
+
+    cout << lowIdx + 1 << " " << hiIdx + 1 << endl;
+}
+
 signed main() {
 #ifdef MAIN_DIR_PATH
     freopen(STRINGIZE_VALUE_OF(MAIN_DIR_PATH)"/input", "r", stdin);
@@ -49,6 +95,11 @@ signed main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
+    int n; cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        solve();
+    }
 
     return 0;
 }
