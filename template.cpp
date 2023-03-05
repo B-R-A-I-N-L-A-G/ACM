27840584efeaf5a2b11168a17280411cbe7350ca
dev-null-undefined <martinkos007@gmail.com>
2023-03-05 23:07:49 +0100

    template rewrite:
    - multiple test files support
    - read/write whole vector or pair at once
    - unordered_set/map alias

--- template.cpp
+++ template.cpp
@@ -5,6 +5,7 @@
 #include <concepts>
 #include <ctime>
 #include <functional>
+#include <filesystem>
 #include <iomanip>
 #include <iostream>
 #include <iostream>
@@ -15,6 +16,7 @@
 #include <queue>
 #include <random>
 #include <ranges>
+#include <regex>
 #include <set>
 #include <set>
 #include <span>
@@ -38,24 +40,79 @@ using namespace std;
 using ll = std::intmax_t;
 using ld = long double;
 using fast = std::int_fast32_t;
-template <typename T> using uset = unordered_set<T>;
-template <typename K, typename V> using umap = unordered_map<K, V>;
+template<typename T> using uset = unordered_set<T>;
+template<typename K, typename V> using umap = unordered_map<K, V>;
 namespace rng = std::ranges;
+
+template<typename T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+    for (const auto &item: v) os << item << " ";
+    return os;
+}
+
+template<typename T>
+istream &operator>>(istream &is, vector<T> &v) {
+    for (auto &item: v) is >> item;
+    return is;
+}
+
+template<typename T, typename K>
+ostream &operator<<(ostream &os, const pair<T, K> &p) {
+    return os << p.first < " " << p.second;
+}
+
+template<typename T, typename K>
+istream &operator>>(istream &is, pair<T, K> &p) { return is >> p.first >> p.second; }
+
 #define int ll
 
-//kubik
+bool runInDebug();
 
-//endregion
+void runTask();
 
+#ifdef MAIN_DIR_PATH
 
+bool runInDebug() {
+    namespace fs = std::filesystem;
+    auto inputRegex = regex("input-\\d{2}");
+
+    auto dir = fs::directory_iterator(STRINGIZE_VALUE_OF(MAIN_DIR_PATH));
+    vector<string> files;
+    for (auto &item: dir) {
+        if (item.path().has_filename()) {
+            const auto &name = item.path().filename();
+            if (!regex_match(name.string(), inputRegex)) continue;
+            files.emplace_back(item.path().string());
+        }
+    }
+
+    sort(files.begin(), files.end());
+    for (string &file: files) {
+        cout << "======================================================================" << endl;
+        cout << "= Running input file " << file << endl;
+        cout << "======================================================================" << endl;
+        freopen(file.c_str(), "r", stdin);
+        runTask();
+    }
+    return true;
+}
+
+#else
+bool runInDebug(){
+    return false;
+}
+#endif
 
 signed main() {
-#ifdef MAIN_DIR_PATH
-    freopen(STRINGIZE_VALUE_OF(MAIN_DIR_PATH)"/input", "r", stdin);
-#endif
+    if (runInDebug())
+        return 0;
+
     ios::sync_with_stdio(false);
     cin.tie(0);
-
-
+    runTask();
     return 0;
+}
+//endregion
+
+void runTask() {
 }
