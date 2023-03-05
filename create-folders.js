75243110a67fa0b85f8a483a120a2de10037ba67
dev-null-undefined <martinkos007@gmail.com>
2023-03-05 12:08:51 +0100

    create-folders added input getter

--- create-folders.js
+++ create-folders.js
@@ -1,8 +1,31 @@
-const generateProblem = (problem, week = '00') => (
+function getInput(problemUrl) {
+    return fetch(problemUrl)
+        .then(response => response.text())
+        .then(html => {
+            const parser = new DOMParser()
+            const doc = parser.parseFromString(html, 'text/html')
+
+            const inputTag = doc.querySelector(".input")
+                .getElementsByTagName("pre")[0];
+            const normalChildren = Array.from(inputTag.childNodes)
+                .filter(node => node.tagName === 'DIV')
+            if (normalChildren.length === 0) {
+                return inputTag.innerText
+            } else {
+                return normalChildren
+                    .map(node => node.innerText)
+                    .join('\n')
+            }
+        })
+}
+
+const generateProblem = (problem, input, week = '00') => (
     `
 mkdir -p week-${week}/${problem}
 cp template.cpp week-${week}/${problem}/main.cpp
-touch week-${week}/${problem}/input
+cat << "HERE-DOC-END" > week-${week}/${problem}/input
+${input}
+HERE-DOC-END
 `
 )
 
@@ -10,30 +33,6 @@ touch week-${week}/${problem}/input
 const getProperName = (problem) => (
     problem.replaceAll(' ', '-').toLowerCase().replaceAll(/[^-a-z]/g, '')
 )
-
-
-const generate = (week = '00') => {
-    const nodes = document.getElementsByClassName('problems')[0]
-        .getElementsByTagName('tbody')[0]
-        .childNodes
-
-    let table_rows = Array.from(nodes)
-    table_rows.shift()
-
-    return table_rows
-            .filter(row => row.tagName === 'TR')
-            .map(problem => ({
-                id: problem.querySelector('td a').innerText,
-                name: problem.querySelector('td div div a').innerText
-            }))
-            .map(({id, name}) => getProperName(id + '-' + name))
-            .reduce((ac, el) => {
-                return ac + generateProblem(el, week)
-            }, '') +
-        `
-./generate-cmake.sh`
-}
-
 
 const getWeek = () => {
     const zerothWeek = new Date(2023, 1, 15)
@@ -44,6 +43,31 @@ const getWeek = () => {
 
     return `${weeks}`.padStart(2, '0')
 }
+
+const generate = async (week = getWeek()) => {
+    const nodes = document.getElementsByClassName('problems')[0]
+        .getElementsByTagName('tbody')[0]
+        .childNodes
+
+    let table_rows = Array.from(nodes)
+    table_rows.shift()
+
+    let resolved = await Promise.all(table_rows
+        .filter(row => row.tagName === 'TR')
+        .map(problem => ({
+            id: problem.querySelector('td a').innerText,
+            url: problem.querySelector('td a').href,
+            name: problem.querySelector('td div div a').innerText
+        }))
+        .map(async ({id, url, name}) => ({name: getProperName(id + '-' + name), input: await getInput(url)})));
+    return resolved
+            .reduce((ac, {name, input}) => {
+                return ac + generateProblem(name, input, week)
+            }, '') +
+        `
+./generate-cmake.sh`
+}
+
 
 // Create an image
 const button = document.createElement("button")
@@ -58,8 +82,8 @@ button.style.border = "1px solid black"
 button.style.borderRadius = "0 0 0 10px"
 button.style.cursor = "pointer"
 
-button.onclick = () => {
-    navigator.clipboard.writeText(generate(getWeek()))
+button.onclick = async () => {
+    navigator.clipboard.writeText(await generate())
 }
 
 document.body.prepend(button)
