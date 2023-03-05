e91a676787cda4f321ed4b7df7f924955df96bc2
Jakub Charvát <charvj12@fit.cvut.cz>
2023-03-05 02:02:14 +0100

    week-02-c-prefixes-and-suffixes: done

--- week-02/c-prefixes-and-suffixes/main.cpp
+++ week-02/c-prefixes-and-suffixes/main.cpp
@@ -43,9 +43,43 @@ template <typename K, typename V> using umap = unordered_map<K, V>;
 namespace rng = std::ranges;
 #define int ll
 
+namespace std {
+template <typename A, typename B>
+struct hash<pair<A, B>> {
+    size_t operator () (const pair<A, B> & p) const {
+        return hash<A>()(p.first) ^ (hash<B>()(p.second) << 1);
+    }
+};
+}
+
 //kubik
 
 //endregion
+
+bool solve() {
+    int n; cin >> n;
+    string a, b;
+    cin >> a >> b;
+
+    umap<pair<char, char>, int> charMap;
+    for (int i = 0; i < n; ++i) {
+        auto p = pair { a[i], b[n - i - 1] };
+        if (p.first > p.second) swap(p.first, p.second);
+        charMap[p] += 1;
+    }
+
+    optional<pair<char, char>> oddPair = nullopt;
+    for (auto [p, count] : charMap) {
+        if (count % 2) {
+            if (oddPair) return false;
+            if (p.first != p.second) return false;
+            oddPair = p;
+        }
+    }
+
+    return true;
+}
+
 signed main() {
 #ifdef MAIN_DIR_PATH
     freopen(STRINGIZE_VALUE_OF(MAIN_DIR_PATH)"/input", "r", stdin);
@@ -53,6 +87,10 @@ signed main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
+    int n; cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cout << (solve() ? "YES" : "NO") << endl;
+    }
 
     return 0;
 }
