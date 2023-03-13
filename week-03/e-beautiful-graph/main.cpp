bca8f04d9fedb3c48ad673d65d13bf6b37977fec
Martin Uždil <uzdilmartin@seznam.cz>
2023-03-13 01:36:35 +0100

    WIP: beautiful graph, you can finish it, Martin or Kuba :), instructions should be clear, peepoSleepo now

--- week-03/e-beautiful-graph/main.cpp
+++ week-03/e-beautiful-graph/main.cpp
@@ -112,7 +112,68 @@ signed main() {
     runTask();
     return 0;
 }
+using Graph = unordered_map<int, unordered_set<int>>;
 //endregion
-
+std::unordered_map<int,size_t> BFS (const Graph & graph, int start, std::unordered_set<int> & visited) {
+    std::unordered_map<int, size_t> distanceMap;
+    queue<int> q;
+    q.push(start);
+    visited.insert(start);
+    distanceMap[start] = 0;
+    while (!q.empty()) {
+        auto vertex = q.front();
+        q.pop();
+        if (!graph.contains(vertex)) continue;
+        for (const auto &neighbour: graph.at(vertex)) {
+            if (!visited.contains(neighbour)) {
+                q.push(neighbour);
+                visited.insert(neighbour);
+                distanceMap[neighbour] = distanceMap[vertex] + 1;
+            }
+        }
+    }
+}
 void runTask() {
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; ++i) {
+        int result = 0;
+        int vertexCount, edges;
+        cin >> vertexCount >> edges;
+        unordered_set<int> vertices(vertexCount);
+        Graph graph;
+        for (int j = 0; j < edges; ++j) {
+            int from, to;
+            cin >> from >> to;
+            graph[from].emplace(to),graph[to].emplace(from);
+            vertices.emplace(from);
+            vertices.emplace(to);
+        }
+        unordered_set<int> visited;
+        for (const auto &item: vertices) {
+            if (!visited.contains(item)) {
+                auto distanceMap = BFS(graph, item, visited);
+
+                //todo: Finish implementing this bullshit:
+//                Algoritmus Bipart
+//                Vstup: Souvislý graf G = (V, E).
+//                (1) Zvol libovolně počáteční vrchol v0.
+//                (2) Pomocí BFS spočti vzdálenosti všech vrcholů od v0.
+//                (3) Pro každou hranu {u, v} ověř, jestli d(v0, u)  != d(v0, v).
+//                (4) Pokud existuje hrana {u, v} tak, že d(v0, u) = d(v0, v):
+//                (5)       return Není bipartitní.
+//                (6) Jinak return Je bipartitní.
+// VIZ courses AG2 - přednáška 2
+
+                //todo: If the component is not bipartite graph, dump it and cout << 0 << endl
+                // If it is, and the graph is not connected (we have multiple bipartite components)
+                // multiply the result from each component
+                // result for each component can be obtained trivially, it's 2^k,
+                // where k is the amount of vertices in the larger bipartite component,
+                // we're counting subsets of k vertices, where each vertex can have number 1 or 3
+                // if the components have same amount of vertices, then it's 2 * 2^k
+            }
+        }
+    }
 }
