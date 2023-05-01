a03b75bd1cb522936d035fdd9922f88a2ec78048
Martin Uždil <uzdilmartin@seznam.cz>
2023-05-01 13:15:39 +0200

    treeeeeeeeeeeee

--- week-10/c-sloth-naptime/main.cpp
+++ week-10/c-sloth-naptime/main.cpp
@@ -106,18 +106,76 @@ signed runTask() {
 #define OPEN_AND_RUN_INPUTS
 #endif
 
+using Graph = vector<vector<int>>;
 //endregion
+struct CTree {
+    struct CNode {
+        CNode(int index, CNode * parent)
+                : m_Index(index), m_Parent(parent) {}
+        int m_Index;
+        CNode * m_Parent;
+        vector<CNode *> m_Children;
+    };
 
-
-
+    CTree (const Graph & adj_list, int root_index) : m_Root(new CNode(root_index, nullptr)) {
+        construct_tree_from_adj_list(m_Root, adj_list);
+    }
+    ~CTree () {
+        delete_node(m_Root);
+    }
+    void delete_node (CNode * ptr) {
+        for (const auto &item: ptr->m_Children) {
+            delete_node(item);
+        }
+        delete ptr;
+    }
+    void construct_tree_from_adj_list(CNode * ptr,const Graph & adj_list) {
+        for (const auto item: adj_list[ptr->m_Index]) {
+            if (item == ptr->m_Index) continue;
+            auto child = new CNode(item, ptr);
+            m_Root->m_Children.push_back(child);
+            construct_tree_from_adj_list(child, adj_list);
+        }
+    }
+    int lca (CNode * u, CNode * v) {
+        while (u != v) {
+            if (m_Levels[u->m_Index] > m_Levels[v->m_Index]) {
+                u = u->m_Parent;
+            } else {
+                v = v->m_Parent;
+            }
+        }
+        return u->m_Index;
+    }
+    void dfs (CNode * node, int level) {
+        m_Levels[node->m_Index] = level;
+        for (const auto &item: node->m_Children) {
+            dfs(item, level+1);
+        }
+    }
+    vector<int> m_Levels;
+    CNode * m_Root;
+};
 signed main() {
     OPEN_AND_RUN_INPUTS
 
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int vertices;
+    cin >> vertices;
+    Graph adj_list;
+    for (int i = 0; i < (vertices - 1); ++i) {
+        int u, v;
+        cin >> u >> v;
+        adj_list[u].push_back(v), adj_list[v].push_back(u);
+    }
+    CTree tree (adj_list, 1);
+    int queries;
+    cin >> queries;
+    for (int i = 0; i < queries; ++i) {
 
-
+    }
     return 0;
 }
 //endregion
